cf4de8806fffdb826cd27eadf9f1107d9542d7bf
JoshuaKiplimo <joshuakiprono8@gmail.com>
2022-10-02 22:19:58 -0400

    Updates

---
+++ precache-manifest.f8d538fddf71b3c3a332faf5349690c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f99d354fa5b5f9a97ceee2c1c827d1e1",
+    "url": "/githubView/index.html"
+  },
+  {
+    "revision": "ca5af03777aaf1cf441b",
+    "url": "/githubView/static/css/main.ff45c5cb.chunk.css"
+  },
+  {
+    "revision": "dd6e754d53fe6f59820a",
+    "url": "/githubView/static/js/2.f88a6716.chunk.js"
+  },
+  {
+    "revision": "d50cbe733d7e1e83d34da7158551fa9c",
+    "url": "/githubView/static/js/2.f88a6716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5af03777aaf1cf441b",
+    "url": "/githubView/static/js/main.33e96408.chunk.js"
+  },
+  {
+    "revision": "486def94c29320552448",
+    "url": "/githubView/static/js/runtime-main.b768bf8e.js"
+  },
+  {
+    "revision": "20a909842ebf13585654be5bada6a463",
+    "url": "/githubView/static/media/login-img.20a90984.svg"
+  },
+  {
+    "revision": "c114fda3847c2b52236772df2f17cd01",
+    "url": "/githubView/static/media/preloader.c114fda3.gif"
+  }
+]);
